a8b15f0178a9cdfbc02a2c0dec62340025e34d64
Parmeet Gill <parmeetgill422@gmail.com>
2025-06-23 16:03:13 -0700

    1

--- capstone-starter/capstone-starter/src/test/java/org/yearup/data/mysql/BaseDaoTestClass.java
+++ capstone-starter/capstone-starter/src/test/java/org/yearup/data/mysql/BaseDaoTestClass.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.yearup.configuration.TestDatabaseConfig;
 
@@ -13,18 +14,18 @@ import java.sql.SQLException;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = TestDatabaseConfig.class)
-public abstract class BaseDaoTestClass
-{
+@TestPropertySource(locations = "classpath:application-test.properties")
+
+public abstract class BaseDaoTestClass {
     @Autowired
     protected DataSource dataSource;
 
     @AfterEach
-    public void rollback() throws SQLException
-    {
-        Connection connection = dataSource.getConnection();
-        if(!connection.getAutoCommit())
-        {
-            dataSource.getConnection().rollback();
+    public void rollback() throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            if (!connection.getAutoCommit()) {
+                connection.rollback();
+            }
         }
     }
 }
