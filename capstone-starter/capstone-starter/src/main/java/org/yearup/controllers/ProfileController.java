965bb3a4faaa7e1da41b6c861bf22aa6eabcf94c
Parmeet Gill <parmeetgill422@gmail.com>
2025-06-25 05:42:18 -0700

    13

--- capstone-starter/capstone-starter/src/main/java/org/yearup/controllers/ProfileController.java
+++ capstone-starter/capstone-starter/src/main/java/org/yearup/controllers/ProfileController.java
@@ -1,4 +1,38 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.yearup.data.ProfileDao;
+import org.yearup.data.UserDao;
+import org.yearup.models.Profile;
+import org.yearup.models.User;
+
+@RestController
+@RequestMapping("/profile")
+@CrossOrigin
 public class ProfileController {
+
+    private final UserDao userDao;
+    private final ProfileDao profileDao;
+
+    @Autowired
+    public PrfileController(UserDao userDao, ProfileDao profileDao){
+        this.userDao = userDao;
+        this.profileDao = profileDao;
+    }
+
+    @GetMapping
+    public Profile getProfile(Authentication authentication){
+        String username = authentication.getName();
+        User user = userDao.getByUserName(username);
+
+        if (user == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+        Profile profile = profileDao.getByUserId(user.get)
+    }
 }
