5e5381c5852ca64ac3eaff586d70ed334dbeaf27
Parmeet Gill <parmeetgill422@gmail.com>
2025-06-25 07:50:53 -0700

    37

--- capstone-starter/capstone-starter/src/main/java/org/yearup/data/ProductDao.java
+++ capstone-starter/capstone-starter/src/main/java/org/yearup/data/ProductDao.java
@@ -1,5 +1,6 @@
 package org.yearup.data;
 
+import org.yearup.models.DuplicateProduct;
 import org.yearup.models.Product;
 
 import java.math.BigDecimal;
@@ -9,9 +10,10 @@ public interface ProductDao
 {
     List<Product> search(Integer categoryId, BigDecimal minPrice, BigDecimal maxPrice, String color);
     List<Product> listByCategoryId(int categoryId);
-    List<String> findDuplicateProductNames();
+    List<DuplicateProduct> findDuplicateProducts();
     Product getById(int productId);
     Product create(Product product);
     void update(int productId, Product product);
     void delete(int productId);
+
 }
