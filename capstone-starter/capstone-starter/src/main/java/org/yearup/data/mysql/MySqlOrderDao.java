ed1f07326070e041ebaac3216074b3419c10eeab
Parmeet Gill <parmeetgill422@gmail.com>
2025-06-25 15:07:50 -0700

    44

--- capstone-starter/capstone-starter/src/main/java/org/yearup/data/mysql/MySqlOrderDao.java
+++ capstone-starter/capstone-starter/src/main/java/org/yearup/data/mysql/MySqlOrderDao.java
@@ -16,11 +16,11 @@ public class MySqlOrderDao extends MySqlDaoBase implements OrderDao {
 
     @Override
     public Order create(Order order) {
-        String sql = "INSERT INTO orders (user_id, order_date) VALUES (?, ?)";
+        String sql = "INSERT INTO orders (user_id) VALUES (?)";
         try (Connection connection = getConnection();
              PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
             statement.setInt(1, order.getUserId());
-            statement.setTimestamp(2, Timestamp.valueOf(order.getOrderDate()));
 
             int rowsAffected = statement.executeUpdate();
             if (rowsAffected > 0) {
@@ -41,6 +41,7 @@ public class MySqlOrderDao extends MySqlDaoBase implements OrderDao {
         String sql = "INSERT INTO order_line_items (order_id, product_id, quantity) VALUES (?, ?, ?)";
         try (Connection connection = getConnection();
              PreparedStatement statement = connection.prepareStatement(sql)) {
+
             statement.setInt(1, orderId);
             statement.setInt(2, productId);
             statement.setInt(3, quantity);
