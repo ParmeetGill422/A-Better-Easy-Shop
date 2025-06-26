b0ec720e10747ba84356fe109690c18269fb5a25
Parmeet Gill <parmeetgill422@gmail.com>
2025-06-26 15:24:23 -0700

    53

--- capstone-starter/capstone-starter/src/main/java/org/yearup/models/Order.java
+++ capstone-starter/capstone-starter/src/main/java/org/yearup/models/Order.java
@@ -1,18 +1,25 @@
 package org.yearup.models;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class Order {
     private int orderId;
     private int userId;
-    private LocalDateTime orderDate;
+
+    private LocalDateTime date;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private BigDecimal shippingAmount;
 
     public Order() {}
 
-    public Order(int orderId, int userId, LocalDateTime orderDate) {
+    public Order(int orderId, int userId) {
         this.orderId = orderId;
         this.userId = userId;
-        this.orderDate = orderDate;
+
     }
 
     public int getOrderId() {
@@ -27,10 +34,22 @@ public class Order {
     public void setUserId(int userId) {
         this.userId = userId;
     }
-    public LocalDateTime getOrderDate() {
-        return orderDate;
-    }
-    public void setOrderDate(LocalDateTime orderDate) {
-        this.orderDate = orderDate;
-    }
+
+    public LocalDateTime getDate() { return date; }
+    public void setDate(LocalDateTime date) { this.date = date; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+
+    public String getState() { return state; }
+    public void setState(String state) { this.state = state; }
+
+    public String getZip() { return zip; }
+    public void setZip(String zip) { this.zip = zip; }
+
+    public BigDecimal getShippingAmount() { return shippingAmount; }
+    public void setShippingAmount(BigDecimal shippingAmount) { this.shippingAmount = shippingAmount; }
 }
