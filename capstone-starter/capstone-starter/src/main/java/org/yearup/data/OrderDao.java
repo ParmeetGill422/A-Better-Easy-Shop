a542e3cb25fa211a4b77da2e0389d9788c65663d
Parmeet Gill <parmeetgill422@gmail.com>
2025-06-26 15:24:11 -0700

    52

--- capstone-starter/capstone-starter/src/main/java/org/yearup/data/OrderDao.java
+++ capstone-starter/capstone-starter/src/main/java/org/yearup/data/OrderDao.java
@@ -2,7 +2,9 @@ package org.yearup.data;
 
 import org.yearup.models.Order;
 
+import java.math.BigDecimal;
+
 public interface OrderDao {
     Order create(Order order);
-    void addOrderLineItem(int orderId, int productId, int quantity);
+    void addOrderLineItem(int orderId, int productId, int quantity, BigDecimal price);
 }
