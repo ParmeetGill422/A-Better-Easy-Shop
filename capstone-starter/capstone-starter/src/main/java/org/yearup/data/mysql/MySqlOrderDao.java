e53e83c04c3c96547cca85d4ac06b7e5c66aa41d
Parmeet Gill <parmeetgill422@gmail.com>
2025-06-26 15:24:34 -0700

    54

--- capstone-starter/capstone-starter/src/main/java/org/yearup/data/mysql/MySqlOrderDao.java
+++ capstone-starter/capstone-starter/src/main/java/org/yearup/data/mysql/MySqlOrderDao.java
@@ -3,8 +3,9 @@ package org.yearup.data.mysql;
 import org.springframework.stereotype.Component;
 import org.yearup.data.OrderDao;
 import org.yearup.models.Order;
-
+import org.yearup.data.ProfileDao;
 import javax.sql.DataSource;
+import java.math.BigDecimal;
 import java.sql.*;
 
 @Component
@@ -14,13 +15,20 @@ public class MySqlOrderDao extends MySqlDaoBase implements OrderDao {
         super(dataSource);
     }
 
+
     @Override
     public Order create(Order order) {
-        String sql = "INSERT INTO orders (user_id) VALUES (?)";
+        String sql = "INSERT INTO orders (user_id, date, address, city, state, zip, shipping_amount) VALUES (?, ?, ?, ?, ?, ?, ?)";
         try (Connection connection = getConnection();
              PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
 
             statement.setInt(1, order.getUserId());
+            statement.setTimestamp(2, Timestamp.valueOf(order.getDate()));
+            statement.setString(3, order.getAddress());
+            statement.setString(4, order.getCity());
+            statement.setString(5, order.getState());
+            statement.setString(6, order.getZip());
+            statement.setBigDecimal(7, order.getShippingAmount());
 
             int rowsAffected = statement.executeUpdate();
             if (rowsAffected > 0) {
@@ -37,14 +45,15 @@ public class MySqlOrderDao extends MySqlDaoBase implements OrderDao {
     }
 
     @Override
-    public void addOrderLineItem(int orderId, int productId, int quantity) {
-        String sql = "INSERT INTO order_line_items (order_id, product_id, quantity) VALUES (?, ?, ?)";
+    public void addOrderLineItem(int orderId, int productId, int quantity, BigDecimal salesPrice) {
+        String sql = "INSERT INTO order_line_items (order_id, product_id, quantity, sales_price, discount) VALUES (?, ?, ?, ?, 0)";
         try (Connection connection = getConnection();
              PreparedStatement statement = connection.prepareStatement(sql)) {
 
             statement.setInt(1, orderId);
             statement.setInt(2, productId);
             statement.setInt(3, quantity);
+            statement.setBigDecimal(4, salesPrice);
             statement.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
